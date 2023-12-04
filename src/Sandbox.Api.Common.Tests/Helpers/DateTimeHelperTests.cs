f5df1faaa620024522671cbc78f487b531ff5bef
cduggan <cduggan@reapit.com>
2023-12-04 10:44:19 +0000

    feat: add common project with DateTimeOffset abstraction

---
+++ src/Sandbox.Api.Common.Tests/Helpers/DateTimeHelperTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Sandbox.Api.Common.Helpers;
+
+namespace Sandbox.Api.Common.Tests.Helpers;
+
+public class DateTimeHelperTests : IDisposable
+{
+    [Fact]
+    public void DateTimeHelper_ShouldReturnConfiguredDate_WhenDateIsSet()
+    {
+        var estOffset = new TimeSpan(4, 0, 0);
+        var testDateTime = new DateTimeOffset(2020, 01, 02, 3, 4, 5, estOffset);
+        
+        DateTimeHelper.Set(testDateTime);
+
+        var now = DateTimeHelper.Now();
+
+        now.Should().Be(testDateTime);
+    }
+    
+    [Fact]
+    public void DateTimeHelper_ShouldNotReturnConfiguredDate_WhenDateIsReSet()
+    {
+        var testDateTime = new DateTimeOffset(2020, 01, 02, 3, 4, 5, new TimeSpan(0));
+        
+        // Set the time to testDateTime
+        DateTimeHelper.Set(testDateTime);
+        
+        // Reset to DateTimeOffset.Now
+        DateTimeHelper.Reset();
+        
+        var now = DateTimeHelper.Now();
+
+        now.Should().BeCloseTo(DateTimeOffset.Now, TimeSpan.FromSeconds(1));
+    }
+    
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private static void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
+        {
+            DateTimeHelper.Reset();
+        }
+    }
+}
---
+++ src/Sandbox.Api.Common/Helpers/DateTimeHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox.Api.Common.Helpers;
+
+public static class DateTimeHelper
+{
+    /// <summary>
+    /// Wrapper around DateTimeOffset.Now, unless otherwise configured using <see cref="Set"/>
+    /// </summary>
+    public static Func<DateTimeOffset> Now = () => DateTimeOffset.Now;
+
+    /// <summary>
+    /// Overrides the default DateTimeOffset.Now response with a custom value
+    /// </summary>
+    /// <param name="dateTimeOffset"></param>
+    public static void Set(DateTimeOffset dateTimeOffset)
+        => Now = () => dateTimeOffset;
+
+    /// <summary>
+    /// Resets the Now response to DateTimeOffset.Now
+    /// </summary>
+    public static void Reset()
+        => Now = () => DateTimeOffset.Now;
+}
