a2b0475e9e2b669909203d290387cc34b8f16887
cduggan <cduggan@reapit.com>
2023-12-11 08:47:13 +0000

    feat: add case insensitive string comparer

---
+++ src/Sandbox.Api.Common.Tests/Helpers/CaseInsensitiveStringEqualityComparerTests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using Sandbox.Api.Common.Helpers;
+
+namespace Sandbox.Api.Common.Tests.Helpers;
+
+public class CaseInsensitiveStringEqualityComparerTests
+{
+    [Fact]
+    public void Equals_ShouldReturnTrue_WhenValuesBothNull()
+    {
+        var comparer = new CaseInsensitiveStringEqualityComparer();
+
+        var result = comparer.Equals(null, null);
+
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("a", null)]
+    [InlineData(null, "b")]
+    public void Equals_ShouldReturnFalse_WhenOneValueNull(string? a, string? b)
+    {
+        var comparer = new CaseInsensitiveStringEqualityComparer();
+
+        var result = comparer.Equals(a, b);
+
+        result.Should().BeFalse();
+    }
+    
+    [Theory]
+    [InlineData("a", "A")]
+    [InlineData("A", "a")]
+    [InlineData("A", "A")]
+    [InlineData("a", "a")]
+    public void Equals_ShouldReturnFalse_WhenValuesEqual(string? a, string? b)
+    {
+        var comparer = new CaseInsensitiveStringEqualityComparer();
+
+        var result = comparer.Equals(a, b);
+
+        result.Should().BeTrue();
+    }
+    
+    [Theory]
+    [InlineData("a", "b")]
+    [InlineData("a", "B")]
+    [InlineData("A", "b")]
+    [InlineData("A", "B")]
+    public void Equals_ShouldReturnFalse_WhenValuesDifferent(string? a, string? b)
+    {
+        var comparer = new CaseInsensitiveStringEqualityComparer();
+
+        var result = comparer.Equals(a, b);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void GetHashCode_ShouldThrowArgumentNullException_WhenObjectNull()
+    {
+        var comparer = new CaseInsensitiveStringEqualityComparer();
+        var action = () => comparer.GetHashCode(null!);
+        action.Should().Throw<ArgumentNullException>();
+    }
+    
+    [Fact]
+    public void GetHashCode_ShouldReturnHashCode_WhenStringProvided()
+    {
+        var comparer = new CaseInsensitiveStringEqualityComparer();
+
+        const string input = "i'm a test!;";
+        var expected = input.GetHashCode();
+
+        var actual = comparer.GetHashCode(input);
+
+        actual.Should().Be(expected);
+    }
+}
---
+++ src/Sandbox.Api.Common/Helpers/CaseInsensitiveStringEqualityComparer.cs
@@ -0,0 +1,23 @@
+﻿namespace Sandbox.Api.Common.Helpers;
+
+public class CaseInsensitiveStringEqualityComparer : IEqualityComparer<string>
+{
+    public bool Equals(string? x, string? y)
+    {
+        if (x == null && y == null)
+            return true;
+
+        if (x == null || y == null)
+            return false;
+
+        return x.Equals(y, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public int GetHashCode(string obj)
+    {
+        if (obj == null)
+            throw new ArgumentNullException(nameof(obj));
+
+        return obj.GetHashCode();
+    }
+}
